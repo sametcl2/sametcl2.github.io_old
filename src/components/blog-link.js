4638c8579e33d7c613e40b6e4614403eebac7ec6
Samet Şahin <samet.sahin@likom.com.tr>
2022-01-22 23:18:14 +0300

    css fixes

--- src/components/blog-link.js
+++ src/components/blog-link.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { Container, DatePublished } from "./blog-link.module.css";
+import {
+  Container,
+  DatePublished,
+  BlogTitle,
+  BlogDate,
+} from "./blog-link.module.css";
 
 const BlogLink = ({ title, datePublished }) => {
   return (
     <div className={Container}>
-      <h2>{title}</h2>
-      <p className={DatePublished}>{datePublished}</p>
+      <h2 id={BlogTitle}>{title}</h2>
+      <p id={BlogDate} className={DatePublished}>
+        {datePublished}
+      </p>
     </div>
   );
 };
